c30f5b04661a360200b65ccc0063a5824f716cfa
Daniel Holzmann <d@velopment.at>
2020-05-02 09:53:00 +0200

    change all data

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,92 +1,97 @@
 module.exports = {
-  siteTitle: 'Hi! I\'m Francisco!',
-  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
+  siteTitle: "Hi! I'm Daniel!",
+  siteDescription: 'I’m Daniel and I’m a full-stack JavaScript engineer!',
   keyWords: ['gatsbyjs', 'react', 'curriculum'],
-  authorName: 'Francisco Santos',
-  twitterUsername: '_franciscodf',
-  githubUsername: 'santosfrancisco',
-  authorAvatar: '/images/avatar.jpeg',
+  authorName: 'Daniel Holzmann',
+  twitterUsername: 'h2online',
+  githubUsername: 'dVelopment',
+  authorAvatar: '/images/avatar.jpg',
   authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
     In 2012 I started working as a support technician and I approached the developers.
     In 2015, I started to study C # and started to contribute with the team giving maintenance in an application in C # and .NET. <br />
     I currently work as a frontend developer and mainly work with <strong>Javascript, NodeJS e React.</strong>`,
   skills: [
     {
-      name: 'HTML',
-      level: 70
-    },
-    {
-      name: 'CSS',
-      level: 60
-    },
-    {
       name: 'Javascript',
-      level: 50
+      level: 95,
     },
     {
       name: 'NodeJs',
-      level: 40
+      level: 95,
     },
     {
       name: 'React',
-      level: 60
+      level: 80,
+    },
+    {
+      name: 'React Navtive',
+      level: 70,
+    },
+    {
+      name: 'HTML',
+      level: 90,
+    },
+    {
+      name: 'CSS',
+      level: 90,
     },
     {
       name: 'Git',
-      level: 70
-    }
+      level: 80,
+    },
   ],
   jobs: [
     {
-      company: "Lendico",
+      company: 'Bond Vet',
       begin: {
-        month: 'apr',
-        year: '2018'
+        month: 'may',
+        year: '2018',
       },
       duration: null,
-      occupation: "Frontend developer",
-      description: "I integrate the Frontend team responsible for developing and maintaining the online lending platform."
-  
-    }, {
-      company: "Anapro",
+      occupation: 'Lead Software Engineer',
+      description:
+        "I'm responsible for developing all our in-house and client facing websites and apps.",
+    },
+    {
+      company: 'Veho technologies',
       begin: {
-        month: 'dec',
-        year: '2016'
+        month: 'jan',
+        year: '2017',
       },
-      duration: '1 yr e 5 mos',
-      occupation: "Fullstack developer",
-      description: "Development and maintenance, corrective and preventive, of web applications for the real estate market."
-    }, {
-      company: "Anapro",
+      duration: '1 year 3 months',
+      occupation: 'Lead Developer',
+      description: 'Development of web, Android and iOS apps.',
+    },
+    {
+      company: 'Anapro',
       begin: {
         month: 'set',
-        year: '2012'
+        year: '2012',
       },
-      duration: '4 yrs e 3 mos',
-      occupation: "Support Technician",
-      description: "Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool."
-  
+      duration: '5 years',
+      occupation: 'Lead Developer',
+      description: 'Consultant/Technical lead as a freelancer.',
     },
     /* ... */
   ],
   social: {
-    twitter: "https://twitter.com/_franciscodf",
-    linkedin: "https://www.linkedin.com/in/santos-francisco",
-    github: "https://github.com/santosfrancisco",
-    email: "yoshi.df@gmail.com"
+    twitter: 'https://twitter.com/h2online',
+    linkedin: 'https://www.linkedin.com/in/daniel-holzmann-bb35235b',
+    github: 'https://github.com/dVelopment',
+    email: 'daniel@danielholzmann.com',
   },
-  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
-  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteUrl: 'https://danielholzmann.com',
+  pathPrefix: '/', // Note: it must *not* have a trailing slash.
   siteCover: '/images/cover.jpeg',
-  googleAnalyticsId: 'UA-000000000-1',
+  googleAnalyticsId: 'UA-159299329-1',
   background_color: '#ffffff',
   theme_color: '#25303B',
   display: 'minimal-ui',
   icon: 'src/assets/gatsby-icon.png',
   headerLinks: [
     {
-      label: 'Francisco Santos',
+      label: 'Daniel Holzmann',
       url: '/',
-    }
-  ]
+    },
+  ],
 }
